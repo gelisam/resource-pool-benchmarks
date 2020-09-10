27c8f5eda6978ce1128d2d028cd94f0613cf2ca5
Jonathan Fischoff <jonathangfischoff@gmail.com>
2020-09-10 15:48:21 -0700

    crazy benchmark that explores a large space of options

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,54 @@
 
+import Data.Pool
+import Control.Concurrent
+import Control.Concurrent.Async
+import Data.IORef
+import Data.Atomics.Counter
+import Control.Monad
+import Data.Time
+import Data.List
+
+data Config = Config
+  { stripes     :: Int
+  , openTime    :: NominalDiffTime
+  , poolSize    :: Int
+  , threadCount :: Int
+  , testTime    :: Int
+  }
+
 main :: IO ()
-main = print "hey"
+main = do
+
+
+  let configs = [ Config {..} |
+          stripes  <- [1 .. 20],
+          openTime <- [60],
+          poolSize <- [1..20],
+          threadCount <- [1 .. 40],
+          testTime <- [10]
+        ]
+
+  putStrLn "Test Time, Open Time, Stripes, Pool Size, Thread Count, Updates"
+
+  mapM_ testRun configs
+
+testRun :: Config -> IO ()
+testRun Config {..} = do
+  pool <- createPool (pure ()) (const $ pure ()) stripes openTime poolSize
+
+  takeCounter <- newCounter 0
+
+  threads <- replicateM threadCount $ forkIO $ forever $ withResource pool $ \_ -> incrCounter_ 1 takeCounter
+
+  threadDelay $ testTime * 1000000
+  mapM_ killThread threads
+
+  let message count = intercalate ","
+        [ show testTime
+        , show openTime
+        , show stripes
+        , show poolSize
+        , show threadCount
+        , show count
+        ]
+  putStrLn . message =<< readCounter takeCounter
