16f18cceccf4e029030e09b6acc491f687b9875d
Samuel Gélineau <gelisam@simspace.com>
2021-03-23 17:38:56 -0400

    abstract over Data.Pool's API

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 
 import Data.Pool
 import Control.Concurrent
@@ -8,38 +9,56 @@ import Data.List
 
 
 data Config = Config
-  { stripes     :: Int
-  , openTime    :: NominalDiffTime
-  , poolSize    :: Int
+  { poolSize    :: Int
   , threadCount :: Int
   , testTime    :: Int
   }
 
+data AbstractPool pool = AbstractPool
+  { abstractCreatePool   :: forall a. IO a -> (a -> IO ()) -> Int -> IO (pool a)
+  , abstractWithResource :: forall a b. pool a -> (a -> IO b) -> IO b
+  , abstractDestroyPool  :: forall a. pool a -> IO ()
+  }
+
+dataDotPool :: AbstractPool Pool
+dataDotPool = AbstractPool
+  { abstractCreatePool = \create destroy poolSize -> do
+      createPool
+        create
+        destroy
+        1    -- 1 stripe
+        0.5  -- keep the resource open for as short as possible
+        poolSize
+  , abstractWithResource = \pool body -> do
+      withResource pool body
+  , abstractDestroyPool = \pool -> do
+      destroyAllResources pool
+  }
+
 main :: IO ()
 main = do
-  testPool $ Config
-    { stripes     = 1
-    , openTime    = 1
-    , poolSize    = 10
-    , threadCount = 100
-    , testTime    = 10
-    }
+  let config = Config
+        { poolSize    = 10
+        , threadCount = 100
+        , testTime    = 10
+        }
+  testAbstractPool config dataDotPool
 
-testPool :: Config -> IO ()
-testPool Config {..} = do
+testAbstractPool :: Config -> AbstractPool pool -> IO ()
+testAbstractPool (Config {..}) (AbstractPool {..}) = do
   totalCounter <- newCounter 0
   let totalize otherCounter =
         flip incrCounter_ totalCounter =<< readCounter otherCounter
-  pool <- createPool (newCounter 0) totalize stripes openTime poolSize
+  pool <- abstractCreatePool (newCounter 0) totalize poolSize
 
-  threads <- replicateM threadCount $ forkIO $ forever $ withResource pool $ \theCounter -> do
+  threads <- replicateM threadCount $ forkIO $ forever $ abstractWithResource pool $ \theCounter -> do
     incrCounter_ 1 theCounter
     yield
 
   threadDelay $ testTime * 1000000
   mapM_ killThread threads
 
-  destroyAllResources pool
+  abstractDestroyPool pool
 
   count <- readCounter totalCounter
   print ("Data.Pool", count)
